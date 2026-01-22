f4a014bf6c3780beb616b3bd8bfb3be1f04ce410
irinapodlisnyuk <irinapodlisnyuk@gmail.com>
2026-01-22 16:25:48 +0300

    work player

--- my-app/src/ts/index.ts
+++ my-app/src/ts/index.ts
@@ -1,7 +1,9 @@
 import { btnUser } from "../ts/btn-user";
 import { cardUser } from "../ts/user-card";
+import { audioPlayer } from "../ts/player";
 
 window.addEventListener("DOMContentLoaded", () => {
   btnUser();
   cardUser();
+  audioPlayer();
 });
